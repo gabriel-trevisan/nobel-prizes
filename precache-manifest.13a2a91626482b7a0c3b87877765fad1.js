e8e6431bb46d64cf86763e76ada14b94614a85b8
Gabriel Trevisan <gabriel.trevisan.santos@gmail.com>
2020-05-30 23:52:58 -0300

    Updates

---
+++ precache-manifest.13a2a91626482b7a0c3b87877765fad1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac95bd3842e4f9886b4112adca9b8b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8731411720ee5f1d2b78",
+    "url": "/static/css/main.3acc9013.chunk.css"
+  },
+  {
+    "revision": "b2bb6bd4c3c07c6fae3a",
+    "url": "/static/js/2.62b3ee5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62b3ee5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8731411720ee5f1d2b78",
+    "url": "/static/js/main.a08ff9e6.chunk.js"
+  },
+  {
+    "revision": "944d8e7f56c56c963238",
+    "url": "/static/js/runtime-main.e2aa004f.js"
+  },
+  {
+    "revision": "34f007adc0f83ee5cfa0bcade9e86bd8",
+    "url": "/static/media/peace.34f007ad.png"
+  },
+  {
+    "revision": "e5033548f3edb9bca4e82ea85a3235a4",
+    "url": "/static/media/physics.e5033548.png"
+  }
+]);
